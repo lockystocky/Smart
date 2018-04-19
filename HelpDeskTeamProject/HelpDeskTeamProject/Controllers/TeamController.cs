2cbf340b59e6c4956c6faa8c874c483bd7520d58
aslobodyanuk <andrew0852@yandex.ru>
2018-04-19 18:38:42 +0300

    Create user roles

---
+++ HelpDeskTeamProject/HelpDeskTeamProject/Controllers/TeamController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using HelpDeskTeamProject.Context;
+using HelpDeskTeamProject.DataModels;
+
+namespace HelpDeskTeamProject.Controllers
+{
+    public class TeamController : Controller
+    {
+        DatabaseContext dbContext = new DatabaseContext();
+
+        public async Task<ActionResult> CreateTeamRole()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> CreateTeamRole(TeamRole newTeamRole)
+        {
+            if (ModelState.IsValid)
+            {
+                dbContext.TeamRoles.Add(newTeamRole);
+            }
+            
+            return View();
+        }
+
+        public async Task<ActionResult> CreateAppRole()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> CreateAppRole(ApplicationRole newAppRole)
+        {
+            if (ModelState.IsValid)
+            {
+                dbContext.AppRoles.Add(newAppRole);
+            }
+
+            return View();
+        }
+    }
+}
