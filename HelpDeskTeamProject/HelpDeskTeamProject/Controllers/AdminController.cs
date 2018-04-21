d0ff4a7cb26aa103334c9d70ea8b478b57cbb6e6
mavortep <redhotchilipeppers.mp@gmail.com>
2018-04-21 06:48:05 +0000

    Added list od users

---
+++ HelpDeskTeamProject/HelpDeskTeamProject/Controllers/AdminController.cs
@@ -0,0 +1,41 @@
+﻿using HelpDeskTeamProject.Context;
+using HelpDeskTeamProject.DataModels;
+using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HelpDeskTeamProject.Controllers
+{
+    public class AdminController : Controller
+    {
+        private AppContext dbContext = new AppContext();
+        
+        // GET: Admin
+        public ActionResult Index()
+        {
+            dynamic compositeModel = new ExpandoObject();
+            compositeModel.Users = GetUsers();
+            compositeModel.TeamRoles = GetTeamRoles();
+            compositeModel.AppRoles = GetAppRoles();
+            return View(compositeModel);
+        }
+
+        private List<User> GetUsers()
+        {
+            return dbContext.Users.ToList();
+        }
+
+        private List<TeamRole> GetTeamRoles()
+        {
+            return dbContext.TeamRoles.ToList();
+        }
+
+        private List<ApplicationRole> GetAppRoles()
+        {
+            return dbContext.AppRoles.ToList();
+        }
+    }
+}
