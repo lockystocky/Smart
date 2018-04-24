a042db069f0d0d72ba0a0d99562c9eaaed82563d
aslobodyanuk <andrew0852@yandex.ru>
2018-04-24 11:01:35 +0300

    Added unit tests project

---
+++ HelpDeskTeamProject/HelpDeskTeamProject.Tests/FirstTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using System.Diagnostics;
+
+namespace HelpDeskTeamProject.Tests
+{
+    [TestFixture]
+    class FirstTest
+    {
+        [Test]
+        public void ShouldPass()
+        {
+            Assert.AreEqual(1, 1);
+        }
+    }
+}
