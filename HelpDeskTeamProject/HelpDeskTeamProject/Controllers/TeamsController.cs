5906181cea57ec7c9a764f0ecafbf1b9d9067176
Milana Ivanchenko <locky.stocky24@gmail.com>
2018-04-26 20:13:12 +0000

    Comments and unnessesary code removed

--- HelpDeskTeamProject/HelpDeskTeamProject/Controllers/TeamsController.cs
+++ HelpDeskTeamProject/HelpDeskTeamProject/Controllers/TeamsController.cs
@@ -28,30 +28,17 @@ namespace HelpDeskTeamProject.Controllers
         {
             this.teamService = teamService;
         }
-
-        //view list of all existing teams
-        public ActionResult AllTeams()
-        {
-            return View(teamService.GetAllTeams());
-        }
+       
         
-        [Authorize]
-        public ActionResult Teams()
-        {
-            return View();
-        }
-
-        //used in view to create teams menu for curent user 
         [Authorize]
         public ActionResult GetCurrentUserTeamsList()
         {            
-           // db.Configuration.ProxyCreationEnabled = false;
-
             var currentUser = GetCurrentUser();
             var currentUserTeamsList = teamService.GetUserTeamsList(currentUser);     
 
             return Json(currentUserTeamsList, JsonRequestBehavior.AllowGet);
         }
+
 
         [Authorize]
         public ActionResult GetCurrentUserName()
@@ -62,17 +49,14 @@ namespace HelpDeskTeamProject.Controllers
             return Json(name, JsonRequestBehavior.AllowGet);
         }
 
-
-
-        //create new team
+        
         [Authorize]
         public ActionResult Create()
         {
             return View();
         }
-
         
-        //create new team
+
         [HttpPost]
         [Authorize]
         [ValidateAntiForgeryToken]
@@ -89,10 +73,6 @@ namespace HelpDeskTeamProject.Controllers
             return View(team);
         }
 
-
-
-        
-
         private User GetCurrentUser()
         {
             var context = new ApplicationDbContext();
@@ -100,12 +80,7 @@ namespace HelpDeskTeamProject.Controllers
             
             return teamService.GetCurrentUser(currentUserId);
         }
-
-
         
-        //Team Administrator must be able to manage Team  members and their roles
-
-
         public ActionResult ManageTeam(int? teamId)
         {
             var currentUser = GetCurrentUser();
@@ -120,6 +95,7 @@ namespace HelpDeskTeamProject.Controllers
 
             return View(viewModel);
         }
+
 
         [HttpPost]
         public ActionResult ChangeUserRoleInTeam(int userId, int teamId, int roleId)
@@ -164,9 +140,7 @@ namespace HelpDeskTeamProject.Controllers
             return Url.Action("ManageTeam", new { teamId = _teamId }); 
         }
 
-        //Team Administrator must be able to invite users into the team by adding user using email address 
-        //and sending automatic invitation with link to registration
-
+        
         [Authorize]
         public ActionResult InviteUser(int? teamId)
         {
@@ -200,8 +174,6 @@ namespace HelpDeskTeamProject.Controllers
             
             return View(viewModel);
         }
-
-       
 
         [Authorize]
         [Route("teams/jointeam/{teamGuid}/{invitedUserId}")]
@@ -242,41 +214,9 @@ namespace HelpDeskTeamProject.Controllers
             return View(viewModel);
         }
 
-        
-
-       /*
-        // GET: Teams/Delete/5
-        public async Task<ActionResult> Delete(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Team team = await db.Teams.FindAsync(id);
-            if (team == null)
-            {
-                return HttpNotFound();
-            }
-            return View(team);
-        }
-
-        // POST: Teams/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<ActionResult> DeleteConfirmed(int id)
-        {
-            Team team = await db.Teams.FindAsync(id);
-            db.Teams.Remove(team);
-            await db.SaveChangesAsync();
-            return RedirectToAction("Index");
-        }*/
-
+      
         protected override void Dispose(bool disposing)
-        {
-            if (disposing)
-            {
-                //db.Dispose();
-            }
+        {            
             base.Dispose(disposing);
         }
     }
