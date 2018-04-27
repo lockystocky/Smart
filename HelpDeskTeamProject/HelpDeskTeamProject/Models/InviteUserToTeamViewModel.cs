354eeeebcbc5f1acd4711cdc1c5a1227acf85db0
mavortep <redhotchilipeppers.mp@gmail.com>
2018-04-27 08:06:04 +0000

    Fixed buttons

--- HelpDeskTeamProject/HelpDeskTeamProject/Models/InviteUserToTeamViewModel.cs
+++ HelpDeskTeamProject/HelpDeskTeamProject/Models/InviteUserToTeamViewModel.cs
@@ -4,7 +4,6 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
-using System.ComponentModel.DataAnnotations;
 
 namespace HelpDeskTeamProject.Models
 {
